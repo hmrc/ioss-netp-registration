30b2db591ab8f3410d4fbfa79c086b60b0f770e3
Andrew Smith <1895818+DevAndyroo@users.noreply.github.com>
2026-05-05 11:27:07 +0100

    VEI-858 copyright and add new test-only endpoint for clearing down al… (#3)
    * VEI-858 copyright and add new test-only endpoint for clearing down all pending registrations
    
    * make it plural

--- app/uk/gov/hmrc/iossnetpregistration/repositories/PendingRegistrationRepository.scala
+++ app/uk/gov/hmrc/iossnetpregistration/repositories/PendingRegistrationRepository.scala
@@ -1,11 +1,23 @@
 /*
  * Copyright 2026 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.iossnetpregistration.repositories
 
 import jakarta.inject.Singleton
+import org.mongodb.scala.bson.BsonDocument
 import org.mongodb.scala.bson.conversions.*
 import org.mongodb.scala.model.*
 import org.mongodb.scala.model.Filters.*
@@ -146,6 +158,13 @@ class PendingRegistrationRepository @Inject()(
   def delete(journeyId: String): Future[Boolean] = {
     collection
       .deleteOne(byJourneyIdOrUrlCode(journeyId))
+      .toFuture()
+      .map(_ => true)
+  }
+  
+  def deleteAll(): Future[Boolean] = {
+    collection
+      .deleteMany(BsonDocument())
       .toFuture()
       .map(_ => true)
   }
