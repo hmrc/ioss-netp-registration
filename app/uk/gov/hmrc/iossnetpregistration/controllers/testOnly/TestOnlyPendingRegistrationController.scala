a07db880dd9212fd756235e7e5cb734a76126b4e
Andrew Smith <1895818+DevAndyroo@users.noreply.github.com>
2026-05-19 12:59:03 +0100

    VEI-487 add testOnly endpoint for creating pending registrations (#4)

--- app/uk/gov/hmrc/iossnetpregistration/controllers/testOnly/TestOnlyPendingRegistrationController.scala
+++ app/uk/gov/hmrc/iossnetpregistration/controllers/testOnly/TestOnlyPendingRegistrationController.scala
@@ -17,19 +17,78 @@
 package uk.gov.hmrc.iossnetpregistration.controllers.testOnly
 
 import jakarta.inject.Inject
-import play.api.libs.json.Json
+import play.api.libs.json.{JsObject, Json}
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
 import uk.gov.hmrc.iossnetpregistration.logging.Logging
+import uk.gov.hmrc.iossnetpregistration.models.des.VatCustomerInfo
+import uk.gov.hmrc.iossnetpregistration.models.{DesAddress, IntermediaryDetails, PendingRegistrationRequest, UserAnswers}
 import uk.gov.hmrc.iossnetpregistration.services.SavePendingRegistrationService
 import uk.gov.hmrc.iossnetpregistration.services.test.TestOnlyActivationCodeService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
-import scala.concurrent.ExecutionContext
+import java.time.{Instant, LocalDate}
+import java.util.UUID
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Random
 
 class TestOnlyPendingRegistrationController @Inject()(
                                                    cc: ControllerComponents,
                                                    pendingRegistrationService: SavePendingRegistrationService
                                                  )(implicit ec: ExecutionContext) extends BackendController(cc) with Logging {
+
+  def createPending(amount: Int, intermediaryNumber: String): Action[AnyContent] = cc.actionBuilder.async {
+    implicit request =>
+      val pendingRegistrations = generatePendingRegistrations(amount, intermediaryNumber)
+
+      Future.sequence(pendingRegistrations.map { (_, pendingRegistration) =>
+        pendingRegistrationService.savePendingRegistration(pendingRegistration)
+      }).map(_ => Ok(Json.toJson(pendingRegistrations.map(_._1))))
+  }
+
+  private def generatePendingRegistrations(amount: Int, intermediaryNumber: String): Seq[(String, PendingRegistrationRequest)] = {
+    (1 to amount).map { index =>
+      val randomVrn = s"44${new Random().between(1000001, 9999999).toString.padTo(7, "1")}"
+      (randomVrn, PendingRegistrationRequest(
+        userAnswers = UserAnswers(
+          id = s"id-$randomVrn",
+          journeyId = UUID.randomUUID().toString,
+          data = Json.parse(s"""{
+                   |    "businessBasedInUK": true,
+                   |    "clientHasVatNumber": true,
+                   |    "clientVatNumber": "$randomVrn",
+                   |    "hasTradingName": false,
+                   |    "businessContactDetails": {
+                   |      "fullName": "fullName",
+                   |      "telephoneNumber": "555999111",
+                   |      "emailAddress": "test@test.com"
+                   |    },
+                   |    "hasFixedEstablishmentInTheEu": false,
+                   |    "previouslyRegistered": false,
+                   |    "websites": [
+                   |      {
+                   |        "site": "www.website.com"
+                   |      }
+                   |    ],
+                   |    "intermediaryDetails": {
+                   |      "intermediaryNumber": "$intermediaryNumber",
+                   |      "intermediaryName": "Intermediary Name"
+                   |    }
+                   |  }
+                   |""".stripMargin).as[JsObject],
+          vatInfo = Some(VatCustomerInfo(
+            registrationDate = Some(LocalDate.now()),
+            desAddress = DesAddress("Line 1", None, None, None, None, Some("AA11 1AA"), "GB"),
+            organisationName = Some("Company name"),
+            singleMarketIndicator = true,
+            individualName = None,
+            deregistrationDecisionDate = None
+          )),
+          lastUpdated = Instant.now()
+        ),
+        intermediaryDetails = IntermediaryDetails(intermediaryNumber, "Perf test automated Int")
+      ))
+    }
+  }
 
   def deletePending(): Action[AnyContent] = cc.actionBuilder.async {
     implicit request =>
